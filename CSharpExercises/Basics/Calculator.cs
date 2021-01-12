1c1b00e432e3f8d991aec222437716395effba4c
Tshepiso <timarape.gmail.com>
2021-01-12 05:54:58 +0200

    Added Average excercise

---
+++ CSharpExercises/Basics/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    /// <summary>
+    /// Write a C# Sharp program that takes four numbers as input to calculate and print the average. Go to the editor
+    ///Test Data:
+    ///Enter the First number: 10
+    ///Enter the Second number: 15
+    ///Enter the third number: 20
+    ///Enter the four number: 30
+    ///Expected Output:
+    ///he average of 10 , 15 , 20 , 30 is: 18
+    /// </summary>
+    public class Calculator
+    {
+        public int Average(int firstNumber, int secondnumber, int thirdNumber, int fourthNumber)
+        {
+            return (firstNumber + secondnumber + thirdNumber + fourthNumber) / 4;
+        }
+
+    }
+}
---
+++ CSharpExercises/Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+
+            Console.Write("Enter the first number :");
+            var firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number :");
+            var secondNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third number :");
+            var thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the fourth number :");
+            var fourthNumber = int.Parse(Console.ReadLine());
+
+            var result = calculator.Average(firstNumber,secondNumber,thirdNumber,fourthNumber);
+
+            Console.WriteLine($"The average of {firstNumber} {secondNumber} {thirdNumber} {fourthNumber} is : {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
