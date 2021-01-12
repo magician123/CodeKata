f0c03a6c455c0f0797653f85226e277e7deb2a2e
Tshepiso <timarape.gmail.com>
2021-01-12 09:42:36 +0200

    Number of ones calculator

--- CSharpExercises/Basics/Calculator.cs
+++ CSharpExercises/Basics/Calculator.cs
@@ -1,27 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Basics
 {
-    /// <summary>
-    /// Write a C# Sharp program that takes four numbers as input to calculate and print the average. Go to the editor
-    ///Test Data:
-    ///Enter the First number: 10
-    ///Enter the Second number: 15
-    ///Enter the third number: 20
-    ///Enter the four number: 30
-    ///Expected Output:
-    ///he average of 10 , 15 , 20 , 30 is: 18
-    /// </summary>
+
     public class Calculator
     {
+        /// <summary>
+        /// Write a C# Sharp program that takes four numbers as input to calculate and print the average. Go to the editor
+        ///Test Data:
+        ///Enter the First number: 10
+        ///Enter the Second number: 15
+        ///Enter the third number: 20
+        ///Enter the four number: 30
+        ///Expected Output:
+        ///he average of 10 , 15 , 20 , 30 is: 18
+        /// </summary>
         public int Average(int firstNumber, int secondnumber, int thirdNumber, int fourthNumber)
         {
             return (firstNumber + secondnumber + thirdNumber + fourthNumber) / 4;
         }
 
+
+        public int NumberOfOnes(int[] array)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 1)
+                    counter++;
+            }
+
+            return counter;
+        }
     }
 }
--- CSharpExercises/Basics/Program.cs
+++ CSharpExercises/Basics/Program.cs
@@ -6,6 +6,31 @@ namespace Basics
     {
         static void Main(string[] args)
         {
+            NumberOfOnesViewer();
+        }
+
+        static void NumberOfOnesViewer()
+        {
+            Calculator calculator = new Calculator();
+
+            Console.Write("how many numbers do whant to enter?");
+            var numberOfIndex =int.Parse(Console.ReadLine());
+
+            int[] array = new int[numberOfIndex];
+
+            for (int i = 0; i < numberOfIndex; i++)
+            {
+                Console.WriteLine($"Enter number {i + 1}");
+                var number = int.Parse(Console.ReadLine());
+
+                array[i] = number;
+            }
+
+            Console.WriteLine($"The number of 1's are {calculator.NumberOfOnes(array)}");
+            Console.ReadLine();
+        }
+        static void AveraageViewier()
+        {
             Calculator calculator = new Calculator();
 
 
@@ -21,7 +46,7 @@ namespace Basics
             Console.Write("Enter the fourth number :");
             var fourthNumber = int.Parse(Console.ReadLine());
 
-            var result = calculator.Average(firstNumber,secondNumber,thirdNumber,fourthNumber);
+            var result = calculator.Average(firstNumber, secondNumber, thirdNumber, fourthNumber);
 
             Console.WriteLine($"The average of {firstNumber} {secondNumber} {thirdNumber} {fourthNumber} is : {result}");
 
